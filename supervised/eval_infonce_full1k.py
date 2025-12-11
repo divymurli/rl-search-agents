dc282c06f8e834501b89ffe68e9283f2e578351c
Ubuntu <ubuntu@ip-172-31-32-84.us-east-2.compute.internal>
2025-12-11 19:31:01 +0000

    minor

--- supervised/eval_infonce_full1k.py
+++ supervised/eval_infonce_full1k.py
@@ -114,7 +114,8 @@ def eval_dev_full_jsonl(
 
 # Defaults you gave (can be overridden via CLI)
 # DEFAULT_CKPT = "/home/ubuntu/rl-search-agents/ckpts/infonce_baseline/step30000.pt"
-DEFAULT_CKPT = "/home/ubuntu/rl-search-agents/ckpts/infonce_margin_no_curriculum/step30000.pt"
+# DEFAULT_CKPT = "/home/ubuntu/rl-search-agents/ckpts/infonce_margin_no_curriculum/step30000.pt"
+DEFAULT_CKPT = "/home/ubuntu/rl-search-agents/ckpts/infonce_group_curriculum_groupk_4/step30000.pt"
 DEFAULT_JSONL = "/home/ubuntu/rl-search-agents/data/rl-search-datasets/candidates_dev_1k_rm3_backoff.jsonl"
 
 
